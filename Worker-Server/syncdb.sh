c9f3d0ccbe66aab9fd004f59d18403ced7b6f907
princemore358 <164121091+princemore358@users.noreply.github.com>
2024-09-03 13:23:48 +0530

    Create syncdb.sh

---
+++ Worker-Server/syncdb.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+
+while true; do
+
+ 
+    echo $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1}') > /root/project-2/db/a_metric
+
+ # make sure to add master ip below
+    rsync /root/project-2/db/* <master-ip>:/root/project-2/db/
+
+    
+    sleep 120
+done
+
+
+
+
